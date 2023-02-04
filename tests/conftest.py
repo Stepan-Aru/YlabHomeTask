e26ed0200ba5f0130f409128e9e6cb20774f0f0f
Stepan-Aru <stepan.aru@gmail.com>
2023-02-04 11:31:52 +0000

    Fixed async tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import pytest
 import pytest_asyncio
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, session
+from sqlalchemy.orm import sessionmaker
 
 from app.database import Base
 from app.main import app
@@ -19,13 +19,12 @@ DB_URL = os.environ.get('DB_URL')
 DB_NAME = os.environ.get('DB_NAME')
 
 
-def execute_db(command: str):
+async def execute_db(command: str):
     conn = None
     try:
         conn = await asyncpg.connect(
             database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_URL,
         )
-        conn.autocommit = True
         await conn.execute(command)
     except Exception as ex:
         print(ex)
@@ -33,16 +32,16 @@ def execute_db(command: str):
         await conn.close()
 
 
-def create_test_db():
-    execute_db(f'CREATE DATABASE test_{DB_NAME};')
+async def create_test_db():
+    await execute_db(f'CREATE DATABASE test_{DB_NAME};')
 
 
-def drop_test_db():
-    execute_db(f'DROP DATABASE IF EXISTS test_{DB_NAME};')
+async def drop_test_db():
+    await execute_db(f'DROP DATABASE IF EXISTS test_{DB_NAME};')
 
 
-def kill_all_connections():
-    execute_db(
+async def kill_all_connections():
+    await execute_db(
         f'SELECT pg_terminate_backend(pg_stat_activity.pid)\n'
         f'FROM pg_stat_activity\n'
         f'WHERE pg_stat_activity.datname = \'test_{DB_NAME}\'\n'
@@ -50,7 +49,7 @@ def kill_all_connections():
     )
 
 
-def create_test_session_local():
+async def create_test_session_local():
     TEST_DB_CONFIG = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_URL}/test_{DB_NAME}'
     test_engine = create_async_engine(TEST_DB_CONFIG, echo=True)
     TestSessionLocal = sessionmaker(
@@ -61,16 +60,21 @@ def create_test_session_local():
         expire_on_commit=False,
     )
 
-    def override_get_db():
+    async def override_get_db():
         db = None
         try:
             db = TestSessionLocal()
             yield db
         finally:
-            db.close()
+            await db.close()
 
     app.dependency_overrides[get_db] = override_get_db
-    Base.metadata.create_all(bind=test_engine)
+    await create_tables(test_engine)
+
+
+async def create_tables(test_engine):
+    async with test_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 @pytest.fixture(scope='session')
@@ -80,14 +84,13 @@ def event_loop(request) -> Generator:
     loop.close()
 
 
-@pytest.fixture(scope='module', autouse=True)
-def create_drop_database():
-    create_test_db()
-    create_test_session_local()
+@pytest_asyncio.fixture(scope='module', autouse=True)
+async def create_drop_database():
+    await create_test_db()
+    await create_test_session_local()
     yield
-    session.close_all_sessions()
-    kill_all_connections()
-    drop_test_db()
+    await kill_all_connections()
+    await drop_test_db()
 
 
 @pytest_asyncio.fixture
